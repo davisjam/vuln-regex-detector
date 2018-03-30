06c32ab04480b0a0c5b8e1158df9d26fbc4b553d
Jamie Davis <davisjam@vt.edu>
2018-03-30 18:10:31 -0400

    cache: testing: reset-db
    Problem:
    During cache testing bugs might pollute the DB with invalid results.
    
    Solution:
    Run reset-db on the mongoDB server to wipe the potentially-corrupt tables.

---
+++ src/cache/server/reset-db.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+/**
+ * Wipe the DB trusted/untrusted tables.
+ * Useful if they have been corrupted by faulty cache-server.js or validate-uploads.js implementations.
+ * ONLY USE DURING TESTING.
+ */
+
+'use strict';
+
+// Modules.
+const fs            = require('fs');
+const MongoClient   = require('mongodb').MongoClient;
+
+// Config.
+if (!process.env.VULN_REGEX_DETECTOR_ROOT) {
+	die(`Error, you must define VULN_REGEX_DETECTOR_ROOT`);
+}
+const configFile = `${process.env.VULN_REGEX_DETECTOR_ROOT}/src/cache/.config.json`;
+const config = JSON.parse(fs.readFileSync(configFile));
+
+// DB info -- convenient shorthand.
+const dbUrl = `mongodb://${config.serverConfig.dbConfig.dbServer}:${config.serverConfig.dbConfig.dbPort}`;
+const dbName = config.serverConfig.dbConfig.dbName;
+const dbLookupCollectionName = config.serverConfig.dbConfig.dbLookupCollection;
+const dbUploadCollectionName = config.serverConfig.dbConfig.dbUploadCollection;
+
+// Connect to DB.
+log(`Connecting to DB ${dbUrl}`);
+MongoClient.connect(dbUrl)
+.then((client) => {
+	log(`Connected`);
+	// Get collection.
+	const db = client.db(dbName);
+
+	const uploadCollection = db.collection(dbUploadCollectionName);
+	const lookupCollection = db.collection(dbLookupCollectionName);
+
+	uploadCollection.drop()
+	.then((result) => {
+		log(`Deleted uploadCollection`);
+		lookupCollection.drop()
+		.then((result) => {
+			log(`Deleted lookupCollection`);
+			return client.close();
+		});
+	})
+	.catch((e) => {
+		log(`Delete error: ${JSON.stringify(e)}`);
+		return client.close();
+	});
+
+})
+.catch((e) => {
+	log(`db error: ${e}`);
+	return Promise.resolve(false);
+});
+
+//////////////////////////////////////
+
+function log(msg) {
+	console.error(new Date().toISOString() + `: ${msg}`);
+}
