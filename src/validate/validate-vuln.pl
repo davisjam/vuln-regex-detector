12aa5ec333f7cfe99c7875da3de2ce16a560e1ba
Jamie Davis <jamiedavis@us.ibm.com>
2019-01-13 13:21:09 -0500

    validate: support querying golang

--- src/validate/validate-vuln.pl
+++ src/validate/validate-vuln.pl
@@ -25,6 +25,7 @@ my %language2validator = (
   "ruby"       => "$pref/ruby/query-ruby.rb",
   "perl"       => "$pref/perl/query-perl.pl",
   "rust"       => "$pref/rust/query-rust",
+  "go"         => "$pref/rust/query-go",
 );
 
 for my $ext (keys %language2validator) {
