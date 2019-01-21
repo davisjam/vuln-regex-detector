e34b2bca90e7c6dec099a85c88c60800e590d066
Jamie Davis <davisjam@vt.edu>
2019-01-21 11:43:46 -0500

    validate: php: choose regex delimiters carefully
    The regex '/abc/' can't be wrapped in '/.../' delimiters.
    Try all supported delimiters according to the PHP docs.
    If none will work, report it is unsupported.

--- src/validate/src/php/query-php.php
+++ src/validate/src/php/query-php.php
@@ -1,10 +1,34 @@
 #!/usr/bin/env php
 <?php
 // Author: Jamie Davis <davisjam@vt.edu>
-// Description: Try REDOS attack on PHP
+// Description: Evaluate a regex in PHP
 
 function my_log($msg) {
   fwrite(STDERR, $msg . "\n");
+}
+
+// Return a string that can be used
+// Returns NULL if nothing could be found
+function patternAsPHPRegex($pat) {
+	//http://php.net/manual/en/regexp.reference.delimiters.php
+	$pairedDelimiters = [
+		['/', '/'],
+		['#', '#'],
+		['`', '`'],
+		['(', ')'],
+		['{', '}'],
+		['[', ']'],
+		['<', '>'],
+	];
+	foreach($pairedDelimiters as $delim) {
+		$first = $delim[0];
+		$last = $delim[1];
+		if (strpos($pat, $first) === FALSE && strpos($pat, $last) === FALSE) {
+			return $first . $pat . $last;
+		}
+	}
+
+	return NULL;
 }
 
 function main() {
@@ -18,43 +42,56 @@ function main() {
   my_log('obj');
 
   // Query regexp.
-  my_log('matching: Pattern /' . $obj->{'pattern'} . '/, input: len ' . strlen($obj->{'input'}));
+  $phpPattern = patternAsPHPRegex($obj->{'pattern'});
+	if (!is_null($phpPattern)) {
+		my_log('matching: pattern ' . $obj->{'pattern'} . ' --> phpPattern ' . $phpPattern);
+		my_log('matching: Pattern ' . $phpPattern . ', input: len ' . strlen($obj->{'input'}));
 
-  $matched = @preg_match('/' . $obj->{'pattern'} . '/', $obj->{'input'}, $matches); // Partial match
-	//var_dump($matches);
-	// NB: (a?)abc|(d)  on "abc" --> (a?) is empty, but (d) is just dropped
+		$matched = @preg_match($phpPattern, $obj->{'input'}, $matches); // Partial match
+		//var_dump($matches);
+		// NB: (a?)abc|(d)  on "abc" --> (a?) is empty, but trailing unused groups like (d) are just dropped
 
-  // capture exception, if any.
-  // will return OK even if there's compilation problems.
-  // PHP 7.4-dev emits a warning unless we @ to ignore it.
-  $except = @array_flip(get_defined_constants(true)['pcre'])[preg_last_error()];
+		// capture exception, if any.
+		// will return OK even if there's compilation problems.
+		// PHP 7.4-dev emits a warning unless we @ to ignore it.
+		$except = @array_flip(get_defined_constants(true)['pcre'])[preg_last_error()];
 
-  // check for compilation
-  $compilation_failed_message = 'preg_match(): Compilation failed:';
-  $last_error = error_get_last();
-  if(strpos($last_error['message'], $compilation_failed_message) !== false) {
-    my_log("caught the invalid input");
-    $except = "INVALID_INPUT";
-		$obj->{'validPattern'} = 0;
-  } else {
-		$obj->{'validPattern'} = 1;
-	}
-
-  // Compose output.
-  $obj->{'matched'} = $matched;
-	if ($matched) {
-		$obj->{'matchContents'} = new stdClass();
-		$obj->{'matchContents'}->{'matchedString'} = $matches[0];
-
-		// Unset any capture groups keyed by name instead of number for consistency with other testers
-		foreach ($matches as $key => $value) {
-			if (!is_int($key)) {
-				unset($matches[$key]);
-			}
+		// check for compilation
+		$compilation_failed_message = 'preg_match(): Compilation failed:';
+		$last_error = error_get_last();
+		if(strpos($last_error['message'], $compilation_failed_message) !== false) {
+			my_log("caught the invalid input");
+			$except = "INVALID_INPUT"; // Override compilation failed
+			$obj->{'validPattern'} = 0;
+		} else {
+			$obj->{'validPattern'} = 1;
 		}
 
-		$obj->{'matchContents'}->{'captureGroups'} = array_slice($matches, 1);
+		// Compose output.
+		$obj->{'matched'} = $matched;
+		if ($matched) {
+			$obj->{'matchContents'} = new stdClass();
+			$obj->{'matchContents'}->{'matchedString'} = $matches[0];
+
+			// Unset any capture groups keyed by name instead of number for consistency with other testers
+			foreach ($matches as $key => $value) {
+				if (!is_int($key)) {
+					unset($matches[$key]);
+				}
+			}
+
+			$obj->{'matchContents'}->{'captureGroups'} = array_slice($matches, 1);
+		}
+	} else {
+		$except = "INVALID_INPUT"; // Override compilation failed
+		$obj->{'validPattern'} = 0;
+		// Dummy values
+		$obj->{'matched'} = 0;
+		$obj->{'matchContents'} = new stdClass();
+		$obj->{'matchContents'}->{'matchedString'} = "";
+		$obj->{'matchContents'}->{'captureGroups'} = [];
 	}
+
   $obj->{'inputLength'} = strlen($obj->{'input'});
   $obj->{'exceptionString'} = $except;
   fwrite(STDOUT, json_encode($obj) . "\n");
